82df93ede1597dd18e740d06f9527081b4e11406
Daniel Xu <danielxu@symphony.csail.mit.edu>
2023-01-17 17:04:56 -0500

    Random Boxes Proposal Generator

---
+++ objdetect/proposals.py
@@ -0,0 +1,82 @@
+import torch
+import random
+
+
+from gdp-objdetect.objdetect.registry import (
+    PROPOSAL_REGISTRY,
+)
+
+@PROPOSAL_REGISTRY.register()
+class RandomBoxes(nn.Module):
+    @configurable
+    def __init__(self):
+        super().__init__()
+
+    def forward(batched_inputs):
+        """
+        Args:
+            batched_inputs: a list, batched outputs of :class:`DatasetMapper` .
+                Each item in the list contains the inputs for one image.
+                For now, each item in the list is a dict that contains:
+                * image: Tensor, image in (C, H, W) format.
+                * instances (optional): groundtruth :class:`Instances`
+                * proposal_boxes (optional): :class:`Instances`, precomputed proposal_boxes.
+                Other information that's included in the original dicts, such as:
+                * "height", "width" (int): the output resolution of the model, used in inference.
+                  See :meth:`postprocess` for details.
+        Returns:
+            list[dict]:
+                Each dict is the output for one input image.
+                The dict contains one key "instances" whose value is a :class:`Instances`.
+                The :class:`Instances` object has the following keys:
+                "pred_boxes", "pred_classes", "scores", "pred_masks", "pred_keypoints"
+        """
+        for image_input in batched_inputs:
+            center_x = random.randint(5, 995)
+            center_y = random.randint(5, 995)
+            box_height = image_input["height"] * random.randint(center_x//2, 999-center_x//2) / 1000.0
+            box_width = image_input["width"] * random.randint(center_y//2, 999-center_y//2) / 1000.0
+            center_x = image_input["height"] * center_x / 1000.0
+            center_y = image_input["width"] * center_y / 1000.0
+            image_input["proposal_boxes"] = torch.Tensor(center_x, center_y, box_height, box_width)
+
+        return batched_inputs
+
+
+@PROPOSAL_REGISTRY.register()
+class NoisedGroundTruth(nn.Module):
+    @configurable
+    def __init__(self):
+        super().__init__()
+
+    @classmethod
+    def from_config(cls, cfg):
+        return {
+            
+        }
+
+    def forward(batched_inputs):
+        """
+        Args:
+            batched_inputs: a list, batched outputs of :class:`DatasetMapper` .
+                Each item in the list contains the inputs for one image.
+                For now, each item in the list is a dict that contains:
+                * image: Tensor, image in (C, H, W) format.
+                * instances (required): groundtruth :class:`Instances`
+                * proposal_boxes (optional): :class:`Instances`, precomputed proposal_boxes.
+                Other information that's included in the original dicts, such as:
+                * "height", "width" (int): the output resolution of the model, used in inference.
+                  See :meth:`postprocess` for details.
+        Returns:
+            batched_inputs: a list, batched outputs of :class:`DatasetMapper` .
+                Each item in the list contains the inputs for one image.
+                For now, each item in the list is a dict that contains:
+                * image: Tensor, image in (C, H, W) format.
+                * instances (required): groundtruth :class:`Instances`
+                * proposal_boxes (required): :class:`Instances`, precomputed proposal_boxes.
+                Other information that's included in the original dicts, such as:
+                * "height", "width" (int): the output resolution of the model, used in inference.
+                  See :meth:`postprocess` for details.
+        """
+
+        return batched_inputs
