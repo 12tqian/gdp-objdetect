e5d90870170406daf4d5ec7b5a3a69f1124f9a4c
12tqian <12tqian@gmail.com>
2023-01-30 22:25:36 -0500

    pray

--- train_net.py
+++ train_net.py
@@ -109,7 +109,8 @@ def do_test(cfg, model, accelerator: Accelerator):
     mapper = ProxModelDatasetMapper(cfg, is_train=True)
     model.eval()
     for dataset_name in cfg.DATASETS.TEST:
-        data_loader = build_detection_test_loader(cfg, dataset_name, mapper=mapper)
+        # data_loader = build_detection_test_loader(cfg, dataset_name, mapper=mapper)
+        data_loader = build_detection_test_loader(cfg, dataset_name)
         # data_loader = accelerator.prepare(data_loader)
         evaluator = get_evaluator(
             cfg, dataset_name, os.path.join(cfg.OUTPUT_DIR, "inference", dataset_name)
