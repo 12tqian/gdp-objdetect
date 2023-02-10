4bf8ac82ed39dfdb3475be9e4765d62ec28be583
Daniel Xu <danielxu@symphony.csail.mit.edu>
2023-02-09 22:09:40 -0500

    Added proportional_nosied_ground_truth

--- objdetect/proposals/__init__.py
+++ objdetect/proposals/__init__.py
@@ -1,3 +1,4 @@
 from .inference_proposal_boxes import InferenceProposalBoxes
 from .noised_ground_truth import NoisedGroundTruth
 from .uniform_random_boxes import UniformRandomBoxes
+from .proportional_noised_ground_truth import ProportionalNoisedGroundTruth
